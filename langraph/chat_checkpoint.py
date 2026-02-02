abb1215094c7a248134a41445e81e634da010fd1
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-02-02 22:00:10 +0500

    Mongo DB for checkpointing installed

---
+++ langraph/chat_checkpoint.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+from typing_extensions import TypedDict
+from typing import Annotated
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
+from langchain.chat_models import init_chat_model
+
+load_dotenv()
+
+llm = init_chat_model(
+    model="gpt-4.1-mini",
+    model_provider="openai"
+)
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+def chat_bot(state: State):
+    print("\n\n\nChat_bot Node Added", state)
+    response = llm.invoke(state.get("messages"))
+    return {"messages": response}
+
+
+graph_builder = StateGraph(State)
+
+graph_builder.add_node("chat_bot", chat_bot)
+
+graph_builder.add_edge(START, "chat_bot")
+graph_builder.add_edge("chat_bot", END)
+
+graph = graph_builder.compile()
+
+updated_state = graph.invoke(State({"messages": ["Hey myself Muhammad Khusham Ali"]}))
+
+print(f"\n\n\nUpdate State: {id(updated_state)}")
+print(f"\n\n\nUpdate State: {updated_state}")
